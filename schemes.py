17162962760ef5a06ecd08c6b5c79a0021685bcc
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2025-01-12 08:02:55 -0400

    data example

--- schemes.py
+++ schemes.py
@@ -42,11 +42,36 @@ class AccessToken(BaseModel):
 
 class Task(BaseModel):
     name: str
-    description: Optional[str] = Field("No description",min_length=5)
+    description: Optional[str] = Field("No description",min_length=5, examples=['Descrip'])
     status: StatusType
     # category: Category
     # user: User
     # tags: List[str] = []
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 123,
+                    "name": "Salvar al mundo",
+                    "description": "Hola Mundo",
+                    "status": StatusType.PENDING,
+                    "tag":["tag 1", "tag 2"],
+                    "category": {
+                        "id": 1234,
+                        "name": "Cate 1"
+                    },
+                    "user": {
+                        "id":12,
+                        "name":"Andres",
+                        "email":"admin@admin.com",
+                        "surname":"Cruz",
+                        "website":"https://desarrollolibre.net"
+                    }
+                }
+            ]
+        }
+    }
 
     
     category_id: int = Field(gt=0)
@@ -63,30 +88,34 @@ class Task(BaseModel):
                 ):
         return cls(name=name, description=description, status=status,category_id=category_id,user_id=user_id)
 
-    class Config:
-        from_attributes=True
+    # class Config:
+    #     from_attributes=True
         
-    class ConfigDict:
-        json_schema_extra = {
-            "example": {
-                "id" : 123,
-                "name": "Salvar al mundo",
-                "description": "Hola Mundo Desc",
-                "status": StatusType.PENDING,
-                "tag":["tag 1", "tag 2"],
-                "category": {
-                    "id":1234,
-                    "name":"Cate 1"
-                },
-                "user": {
-                    "id":12,
-                    "name":"Andres",
-                    "email":"admin@admin.com",
-                    "surname":"Cruz",
-                    "website":"http://desarrollolibre.net",
-                }
-            }
-        }
+    # class ConfigDict: # NO funciona
+    #     model_config = {
+    #     "json_schema_extra": {
+    #         "examples": [
+    #             {
+    #                 "id": 123,
+    #                 "name": "Salvar al mundo",
+    #                 "description": "Hola Mundo",
+    #                 "status": StatusType.PENDING,
+    #                 "tag":["tag 1", "tag 2"],
+    #                 "category": {
+    #                     "id": 1234,
+    #                     "name": "Cate 1"
+    #                 },
+    #                 "user": {
+    #                     "id":12,
+    #                     "name":"Andres",
+    #                     "email":"admin@admin.com",
+    #                     "surname":"Cruz",
+    #                     "website":"https://desarrollolibre.net"
+    #                 }
+    #             }
+    #         ]
+    #     }
+    # }
 
     @field_validator('name') #validator
     def name_alphanumeric_and_whitespace(cls, v):
