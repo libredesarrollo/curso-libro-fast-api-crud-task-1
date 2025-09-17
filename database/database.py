9d0e7113ad5c7883c435a979b4984779bc682659
Andres Cruz <desarrollolibre.net@gmail.com>
2025-09-17 15:46:43 -0400

    deploy

--- database/database.py
+++ database/database.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+mysqlconnector://root:password@localhost:3306/fastapi"
-DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/fastapi"
+# DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/fastapi"
+DATABASE_URL = "mysql+pymysql://root@localhost:3306/fastapi"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
