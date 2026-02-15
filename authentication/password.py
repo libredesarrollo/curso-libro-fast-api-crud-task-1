fc34d457b0dbf53b69e47c99089f4d7b2c3f0266
andres <desarrollolibre.net@gmail.com>
2026-02-15 06:41:58 -0400

    Password generar moderno

--- authentication/password.py
+++ authentication/password.py
@@ -1,14 +1,21 @@
 
+import bcrypt
 import secrets
-from passlib.context import CryptContext
 
-pwd_context = CryptContext( schemes=["bcrypt"])
 
-def get_password_hash(password: str) -> str: 
-    return pwd_context.hash(password)
+def get_password_hash(password: str) -> str:
+    # Convertimos la contraseña a bytes
+    pwd_bytes = password.encode('utf-8')
+    # Generamos el salt y el hash
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+    # Retornamos como string para guardar en la BD
+    return hashed_password.decode('utf-8')
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    password_byte_enc = plain_password.encode('utf-8')
+    hashed_password_enc = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password_byte_enc, hashed_password_enc)
 
 def generate_token() -> str:
     return secrets.token_urlsafe(32)
---
+++ authentication/password_old.py
@@ -0,0 +1,14 @@
+
+import secrets
+from passlib.context import CryptContext
+
+pwd_context = CryptContext( schemes=["bcrypt"])
+
+def get_password_hash(password: str) -> str: 
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def generate_token() -> str:
+    return secrets.token_urlsafe(32)
