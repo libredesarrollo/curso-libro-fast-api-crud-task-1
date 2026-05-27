e5ff17688b6fab3ddf11a3c58a011749c995edb1
andres <desarrollolibre.net@gmail.com>
2026-05-27 10:35:14 -0400

    eliminado scarlette

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 # Demo mode is active by default. Set DEMO_MODE=false or DEMO_MODE=0 in your env to disable.
 DEMO_MODE = os.getenv("DEMO_MODE", "true").lower() not in ("false", "0", "no", "off")
+
